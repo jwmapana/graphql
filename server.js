5eda9fd761bf30ea2402757b46dfb4fe60d156af
jwmcneely <jim@envisiondata.com>
2018-01-12 16:48:47 -0800

    graphql now talking to vue.js successfully. handles OPTIONS requests correctly.

--- server.js
+++ server.js
@@ -7,9 +7,28 @@ const GRAPHQL_PORT = 3000;
 
 const graphQLServer = express();
 
+graphQLServer.use(function(req,res,next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // console.log('req', req);
+
+  if ('OPTIONS' === req.method) {
+    console.log('entered OPTIONS branch');
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
+});
+
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
 ));
+
+//http://192.168.33.10:3000/graphql?
+//http://admin3.apana.us:3000/graphql?
